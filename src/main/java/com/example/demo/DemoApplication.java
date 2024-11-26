fe8e0b528cc2a7aebb6fcd5cbc5f8f2094a13872
Jakub Knap <jknap40@gmail.com>
2024-11-26 18:36:33 +0100

    Lokalna zmiana w projekcie

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         System.out.println("Witamy w aplikacji Spring Boot!");
         System.out.println("To gałąź mojaFunkcja - rozwijana druga funkcja.");
+        System.out.println("Kolejna zmiana");
         SpringApplication.run(DemoApplication.class, args);
     }
 
