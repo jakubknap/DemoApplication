a8a944df8c93359b819d5e76f4c97167239570c6
Jakub Knap <jknap40@gmail.com>
2024-11-26 18:06:35 +0100

    Dodano DrugiKontroler i nazwę gałęzi do metody main()

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         System.out.println("Witamy w aplikacji Spring Boot!");
+        System.out.println("Aktualna gałąź: devF2");
         SpringApplication.run(DemoApplication.class, args);
     }
 
