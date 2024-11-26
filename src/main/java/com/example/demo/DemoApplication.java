e07d48b2399ee07ae035ff8ee03118b91c59aeb2
Jakub Knap <jknap40@gmail.com>
2024-11-26 17:55:59 +0100

    Dodano nową funkcję w gałęzi mojaFunkcja

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         System.out.println("Witamy w aplikacji Spring Boot!");
+        System.out.println("To gałąź mojaFunkcja - rozwijana druga funkcja.");
         SpringApplication.run(DemoApplication.class, args);
     }
 
