e9d360e53961f4fd5e93de803a7bf0a8112c952a
Jakub Knap <jknap40@gmail.com>
2024-11-26 17:43:47 +0100

    Added welcome message in main()

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.out.println("Witamy w aplikacji Spring Boot!");
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
