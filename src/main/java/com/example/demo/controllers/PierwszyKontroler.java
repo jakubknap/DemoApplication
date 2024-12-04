7046e48735825f73605018bdbb2bb7600368a357
Jakub Knap <jknap40@gmail.com>
2024-12-04 17:18:35 +0100

    Dodanie endpointu do testu drugiej aplikacji

--- src/main/java/com/example/demo/controllers/PierwszyKontroler.java
+++ src/main/java/com/example/demo/controllers/PierwszyKontroler.java
@@ -1,4 +1,15 @@
 package com.example.demo.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class PierwszyKontroler {
+
+    @GetMapping
+    public String helloWorld() {
+        return "Hello World from demo application";
+    }
 }
